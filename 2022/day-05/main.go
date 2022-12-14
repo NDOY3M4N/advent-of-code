6b7401e6c53cafcc37f2ccc7c7322c98c2d0d114
Abdoulaye NDOYE <pa.ndoye@outlook.fr>
2022-12-14 20:30:14 +0000

    feat(day05): finish part 2 and refactore a little

--- 2022/day-05/main.go
+++ 2022/day-05/main.go
@@ -7,6 +7,8 @@ import (
 	"strings"
 )
 
+// Wanted to test my knowledge of Stack data struct
+// So far it is working for the part 1 but for the part 2 ...
 type Stack struct {
 	items []string
 }
@@ -32,8 +34,22 @@ func (s *Stack) GetTop() string {
 	return s.items[lastIndex]
 }
 
-// TODO: automate this process, don't be lazy
-// I will manually create the Stacks
+func (s *Stack) Pop9001(qty int) (itemsToMove []string) {
+	l := len(s.items) - qty
+	itemsToMove = s.items[l:]
+
+  s.items = s.items[:l]
+	return itemsToMove
+}
+
+func (s *Stack) Push9001(items []string) {
+	for _, item := range items {
+		s.Push(item)
+	}
+}
+
+// TODO: automate this process, don't be lazy create a func to parse it.
+// I will manually create the Stacks (crates)
 var myStacks = map[int]*Stack{
 	1: {items: []string{"B", "G", "S", "C"}},
 	2: {items: []string{"T", "M", "W", "H", "J", "N", "V", "G"}},
@@ -57,15 +73,18 @@ func main() {
 	parsedString := strings.Split(string(file), "\n\n")
 	commands := strings.Split(parsedString[1], "\n")
 
-	// Crane operation
 	for _, command := range commands {
 		// Somehow I have an empty line added to the slice
 		if len(command) > 0 {
-			operateCrane(parseCommand(command))
+			qty, from, to := parseCommand(command)
+			// Part 1
+			// operateCrateMover9000(myStacks, qty, from, to)
+			// Part 2
+			operateCrateMover9001(myStacks, qty, from, to)
 		}
 	}
 
-	fmt.Println("The crates that end up at the top are:", topCrates())
+	fmt.Println("The crates that end up at the top are:", topCrates(myStacks))
 }
 
 func parseCommand(c string) (qty, from, to int) {
@@ -76,21 +95,23 @@ func parseCommand(c string) (qty, from, to int) {
 	return
 }
 
-func operateCrane(qty, from, to int) {
+func operateCrateMover9000(s map[int]*Stack, qty, from, to int) {
 	for qty > 0 {
-		itemToMove := myStacks[from].Pop()
-		myStacks[to].Push(itemToMove)
+		itemToMove := s[from].Pop()
+		s[to].Push(itemToMove)
 
 		qty--
 	}
 }
 
-func topCrates() (result string) {
-  i := 1
-	for i <= len(myStacks) {
-		result += myStacks[i].GetTop()
+func operateCrateMover9001(s map[int]*Stack, qty, from, to int) {
+	itemsToMove := s[from].Pop9001(qty)
+	s[to].Push9001(itemsToMove)
+}
 
-    i++
+func topCrates(s map[int]*Stack) (result string) {
+	for i := 1; i <= len(myStacks); i++ {
+		result += s[i].GetTop()
 	}
 
 	return
